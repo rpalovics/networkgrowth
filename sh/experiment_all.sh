c4197a78a3cd72043a787292879368b6aa707aac
Robert Palovics <rpalovics@info.ilab.sztaki.hu>
2017-01-02 15:13:15 +0100

    experiment_all update

--- sh/experiment_all.sh
+++ sh/experiment_all.sh
@@ -1,6 +1,6 @@
 DATA_FOLDER="/home/rpalovics/networkgrowth/data"
 STATS_FOLDER="home/rpalovics/networkgrowth/data/stats"
 bash experiment_random_shuffle.sh $DATA_FOLDER
-bash experiment_degree_distribution_evolution.sh $DATA_FOLDER $OUT_FOLDER
-bash experiment_exponent_stats.sh $OUT_FOLDER
-bash experiment_network_events.sh $DATA_FOLDER $OUT_FOLDER
+bash experiment_degree_distribution_evolution.sh $DATA_FOLDER $STATS_FOLDER
+bash experiment_exponent_stats.sh $STATS_FOLDER
+bash experiment_network_events.sh $DATA_FOLDER $STATS_FOLDER
